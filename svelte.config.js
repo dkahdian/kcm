e11bc6eee41131b45cdf4d2a6c7a7acbfd8d5557
David Kahdian <dkahdian@g.ucla.edu>
2025-09-29 10:17:46 -0700

    Fix deployment to fall back on index.html rather than 404.html for Github pages

--- svelte.config.js
+++ svelte.config.js
@@ -12,8 +12,8 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter({
-				// For GitHub Pages SPA routing, use a 404.html fallback
-				fallback: '404.html'
+				// Generate index.html as the main entry point, with 404.html for SPA routing
+				fallback: 'index.html'
 			})
 	}
 };

0ccf377b8d0231fe620330dfe08357ab54ec8636
David Kahdian <dkahdian@g.ucla.edu>
2025-10-09 15:53:14 -0700

    Fixed bug with redundant checkmarks on filters

--- src/lib/FilterDropdown.svelte
+++ src/lib/FilterDropdown.svelte
@@ -153,11 +153,6 @@
                   class="filter-checkbox"
                 />
                 <span class="item-name">{filter.name}</span>
-                {#if isFilterChecked(filter)}
-                  <svg width="16" height="16" viewBox="0 0 16 16" fill="currentColor" class="check-icon">
-                    <path d="M13.854 3.646a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708 0l-3.5-3.5a.5.5 0 1 1 .708-.708L6.5 10.293l6.646-6.647a.5.5 0 0 1 .708 0z"/>
-                  </svg>
-                {/if}
               </label>
             {/each}
           </div>

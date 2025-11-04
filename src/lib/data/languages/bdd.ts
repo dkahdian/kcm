d68067090fccbf03c88e85e0ba504095cba1f083
bot <b@o.t>
2025-11-04 17:23:48 +0000

    Data contribution: edit language

--- src/lib/data/languages/bdd.ts
+++ src/lib/data/languages/bdd.ts
@@ -9,24 +9,24 @@ export const bdd: KCLanguage = {
   descriptionRefs: ['Darwiche_2002'],
   properties: {
     queries: {
-      CO: { polytime: 'exp', refs: ['Darwiche_2002'] },
-      VA: { polytime: 'exp', refs: ['Darwiche_2002'] },
       CE: { polytime: 'exp', refs: ['Darwiche_2002'] },
-      IM: { polytime: 'exp', refs: ['Darwiche_2002'] },
-      EQ: { polytime: 'exp', refs: ['Darwiche_2002'] },
-      SE: { polytime: 'exp', refs: ['Darwiche_2002'] },
+      CO: { polytime: 'quasi', note: 'TEST', refs: ['Darwiche_2002'] },
       CT: { polytime: 'exp', refs: ['Darwiche_2002'] },
-      ME: { polytime: 'exp', refs: ['Darwiche_2002'] }
+      EQ: { polytime: 'exp', refs: ['Darwiche_2002'] },
+      IM: { polytime: 'exp', refs: ['Darwiche_2002'] },
+      ME: { polytime: 'exp', refs: ['Darwiche_2002'] },
+      SE: { polytime: 'exp', refs: ['Darwiche_2002'] },
+      VA: { polytime: 'exp', refs: ['Darwiche_2002'] }
     },
     transformations: {
+      AND_BC: { polytime: 'poly', refs: ['Darwiche_2002'] },
+      AND_C: { polytime: 'not-poly-conditional', note: 'Unless P=NP', refs: ['Darwiche_2002'] },
       CD: { polytime: 'poly', refs: ['Darwiche_2002'] },
       FO: { polytime: 'not-poly-conditional', note: 'Unless P=NP', refs: ['Darwiche_2002'] },
-      SFO: { polytime: 'poly', refs: ['Darwiche_2002'] },
-      AND_C: { polytime: 'not-poly-conditional', note: 'Unless P=NP', refs: ['Darwiche_2002'] },
-      AND_BC: { polytime: 'poly', refs: ['Darwiche_2002'] },
-      OR_C: { polytime: 'not-poly-conditional', note: 'Unless P=NP', refs: ['Darwiche_2002'] },
+      NOT_C: { polytime: 'poly', refs: ['Darwiche_2002'] },
       OR_BC: { polytime: 'poly', refs: ['Darwiche_2002'] },
-      NOT_C: { polytime: 'poly', refs: ['Darwiche_2002'] }
+      OR_C: { polytime: 'not-poly-conditional', note: 'Unless P=NP', refs: ['Darwiche_2002'] },
+      SFO: { polytime: 'poly', refs: ['Darwiche_2002'] }
     }
   },
   tags: [

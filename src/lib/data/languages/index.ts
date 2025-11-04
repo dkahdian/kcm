fd60f26bc300b1fe0d0c74b522063f7f87db50a8
bot <b@o.t>
2025-11-04 00:37:51 +0000

    Data contribution: references references

--- src/lib/data/languages/index.ts
+++ src/lib/data/languages/index.ts
@@ -1,55 +1,54 @@
-// Export all language modules
-export { nnf } from './nnf.js';
+export { bdd } from './bdd.js';
+export { cnf } from './cnf.js';
+export { dDnnf } from './d-dnnf.js';
+export { dnf } from './dnf.js';
 export { dnnf } from './dnnf.js';
 export { dNnf } from './d-nnf.js';
-export { sNnf } from './s-nnf.js';
-export { fNnf } from './f-nnf.js';
-export { dDnnf } from './d-dnnf.js';
-export { sdDnnf } from './sd-dnnf.js';
-export { bdd } from './bdd.js';
 export { fbdd } from './fbdd.js';
-export { obdd } from './obdd.js';
-export { obddLt } from './obdd-lt.js';
-export { dnf } from './dnf.js';
-export { cnf } from './cnf.js';
-export { pi } from './pi.js';
+export { fNnf } from './f-nnf.js';
 export { ip } from './ip.js';
 export { mods } from './mods.js';
+export { nnf } from './nnf.js';
+export { obdd } from './obdd.js';
+export { obddLt } from './obdd-lt.js';
+export { pi } from './pi.js';
+export { sdDnnf } from './sd-dnnf.js';
+export { sNnf } from './s-nnf.js';
 
-// Convenience array of all languages
-import { nnf } from './nnf.js';
+import { bdd } from './bdd.js';
+import { cnf } from './cnf.js';
+import { dDnnf } from './d-dnnf.js';
+import { dnf } from './dnf.js';
 import { dnnf } from './dnnf.js';
 import { dNnf } from './d-nnf.js';
-import { sNnf } from './s-nnf.js';
-import { fNnf } from './f-nnf.js';
-import { dDnnf } from './d-dnnf.js';
-import { sdDnnf } from './sd-dnnf.js';
-import { bdd } from './bdd.js';
 import { fbdd } from './fbdd.js';
-import { obdd } from './obdd.js';
-import { obddLt } from './obdd-lt.js';
-import { dnf } from './dnf.js';
-import { cnf } from './cnf.js';
-import { pi } from './pi.js';
+import { fNnf } from './f-nnf.js';
 import { ip } from './ip.js';
 import { mods } from './mods.js';
+import { nnf } from './nnf.js';
+import { obdd } from './obdd.js';
+import { obddLt } from './obdd-lt.js';
+import { pi } from './pi.js';
+import { sdDnnf } from './sd-dnnf.js';
+import { sNnf } from './s-nnf.js';
+
 import type { KCLanguage } from '../../types.js';
 
 export const allLanguages: KCLanguage[] = [
-  nnf,
+  bdd,
+  cnf,
+  dDnnf,
+  dnf,
   dnnf,
   dNnf,
-  sNnf,
-  fNnf,
-  dDnnf,
-  sdDnnf,
-  bdd,
   fbdd,
+  fNnf,
+  ip,
+  mods,
+  nnf,
   obdd,
   obddLt,
-  dnf,
-  cnf,
   pi,
-  ip,
-  mods
+  sdDnnf,
+  sNnf
 ];

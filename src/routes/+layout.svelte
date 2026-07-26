e4ebe51d3ba630b9f21319de00d128d642debf59
David Kahdian <dkahdian@g.ucla.edu>
2026-07-26 09:06:52 -0700

    Temporaril removed StarAI bear for anonymization purposes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
-	import starai from '$lib/assets/starai.png';
 	import '../app.css';
 
 	let { children } = $props();
 </script>
-
-<svelte:head>
-	<link rel="icon" href={starai} />
-</svelte:head>
 
 {@render children?.()}
